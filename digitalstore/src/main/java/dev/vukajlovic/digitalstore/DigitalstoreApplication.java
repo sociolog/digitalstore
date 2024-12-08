f24408843b6ff06da11450ebbbc1511b7ffaf724
sociolog <mile.vukajlovic@outlook.com>
2024-12-08 22:17:47 +0100

    Initialize Spring Boot project for digital store
    This commit sets up a new Spring Boot application under the "digitalstore" module. It includes a Maven wrapper, a sample application with configuration for PostgreSQL, and basic dependencies such as Spring Boot Starter Web, JPA, and Lombok. Initial test scaffolding and a basic application class are also added.

---
+++ digitalstore/src/main/java/dev/vukajlovic/digitalstore/DigitalstoreApplication.java
@@ -0,0 +1,13 @@
+package dev.vukajlovic.digitalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalstoreApplication.class, args);
+	}
+
+}
---
+++ digitalstore/src/test/java/dev/vukajlovic/digitalstore/DigitalstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vukajlovic.digitalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
